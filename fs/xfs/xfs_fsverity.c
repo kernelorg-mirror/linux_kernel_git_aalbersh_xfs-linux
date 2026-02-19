a488a8c9f9c6aa22a1899a11f6f1924d0b0dccb1
Andrey Albershteyn <aalbersh@kernel.org>
2026-02-19 10:53:56 +0100

    xfs: remove unwritten extents after preallocations in fsverity metadata
    XFS preallocates spaces during writes. In normal I/O this space, if
    unused, is removed by truncate. For files with fsverity XFS does not use
    truncate as fsverity metadata is stored past EOF.
    
    After we're done with writing fsverity metadata iterate over extents in
    that region and remove any unwritten ones. These would be left overs in
    the holes in the merkle tree and past fsverity descriptor.
    
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

--- fs/xfs/xfs_fsverity.c
+++ fs/xfs/xfs_fsverity.c
@@ -12,12 +12,15 @@
 #include "xfs_inode.h"
 #include "xfs_log_format.h"
 #include "xfs_trans.h"
+#include "xfs_defer.h"
 #include "xfs_trace.h"
 #include "xfs_quota.h"
 #include "xfs_fsverity.h"
 #include "xfs_iomap.h"
 #include "xfs_error.h"
 #include "xfs_health.h"
+#include "xfs_bmap.h"
+#include "xfs_bmap_util.h"
 #include <linux/fsverity.h>
 #include <linux/iomap.h>
 #include <linux/pagemap.h>
@@ -179,6 +182,74 @@ err_cancel:
 	return error;
 }
 
+static int
+xfs_fsverity_cancel_unwritten(
+	struct xfs_inode	*ip,
+	loff_t			start,
+	loff_t			end)
+{
+	struct xfs_mount	*mp = ip->i_mount;
+	struct xfs_trans	*tp;
+	xfs_fileoff_t		offset_fsb = XFS_B_TO_FSBT(mp, start);
+	xfs_fileoff_t		end_fsb = XFS_B_TO_FSB(mp, end);
+	struct xfs_bmbt_irec	imap;
+	int			nimaps;
+	int			error = 0;
+	int			done;
+
+	while (offset_fsb < end_fsb) {
+		nimaps = 1;
+
+		error = xfs_trans_alloc(mp, &M_RES(mp)->tr_write, 0, 0, 0, &tp);
+		if (error)
+			return error;
+
+		xfs_ilock(ip, XFS_ILOCK_EXCL);
+		error = xfs_bmapi_read(ip, offset_fsb,
+				end_fsb - offset_fsb, &imap, &nimaps,
+				0);
+		if (error)
+			goto out_cancel;
+
+		if (nimaps == 0)
+			goto out_cancel;
+
+		if (imap.br_state == XFS_EXT_UNWRITTEN) {
+			done = 0;
+
+			xfs_trans_ijoin(tp, ip, 0);
+
+			while (!done) {
+				error = xfs_bunmapi(tp, ip, imap.br_startoff,
+						imap.br_blockcount, 0, 1,
+						&done);
+				if (error)
+					goto out_cancel;
+
+				error = xfs_defer_finish(&tp);
+				if (error)
+					goto out_cancel;
+			}
+
+			error = xfs_trans_commit(tp);
+			if (error)
+				goto out_unlock;
+		} else {
+			xfs_trans_cancel(tp);
+		}
+		xfs_iunlock(ip, XFS_ILOCK_EXCL);
+
+		offset_fsb = imap.br_startoff + imap.br_blockcount;
+	}
+
+	return error;
+out_cancel:
+	xfs_trans_cancel(tp);
+out_unlock:
+	xfs_iunlock(ip, XFS_ILOCK_EXCL);
+	return error;
+}
+
 
 /*
  * Prepare to enable fsverity by clearing old metadata.
@@ -259,6 +330,14 @@ xfs_fsverity_end_enable(
 	 */
 	error = filemap_write_and_wait_range(inode->i_mapping, range_start,
 			LLONG_MAX);
+	if (error)
+		goto out;
+
+	/*
+	 * Remove unwritten extents left by COW preallocations and write
+	 * preallocation in the merkle tree holes and past descriptor
+	 */
+	error = xfs_fsverity_cancel_unwritten(ip, range_start, LLONG_MAX);
 	if (error)
 		goto out;
 
